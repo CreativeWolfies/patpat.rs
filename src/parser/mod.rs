ef9b2c8c93dfaba0e69d2482e8e2fc5139b7d5d8
Adrien Burgun <adrien.burgun@orange.fr>
2020-01-07 22:52:15 +0100

    :art: Cached regex parsing

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -6,29 +6,32 @@ use std::{
 pub fn parse(raw: String) {
     let lines: Vec<&str> = raw.split('\n').collect();
     let mut terms: Vec<Term> = Vec::new();
-    for line in lines.iter() {
-        let mut trimmed_line = line.clone();
+
+    // generate the Regex objects from the MATCHERS array
+    let mut regexes: Vec<(patpat::Kinds, Regex)> = Vec::new();
+    for matcher in MATCHERS.iter() {
+        regexes.push((matcher.0, match Regex::new(matcher.1) {
+            Ok(val) => val,
+            _ => {
+                eprintln!("     __\n  _ / /\n (_) | \n   | | \n  _| | \n (_) | \n    \\_\\\n"); // x3
+                process::exit(1);
+            },
+        }));
+    }
+
+    for line in lines.iter() { // for each line
+        let mut trimmed_line = line.clone(); // a copy of the line, which progressively gets trimmed
         while trimmed_line.len() > 0 {
             let mut matched = false;
-            for matcher in MATCHERS.iter() {
-                let regex = match Regex::new(matcher.1) {
-                    Ok(val) => val,
-                    _ => {
-                        eprintln!("     __\n  _ / /\n (_) | \n   | | \n  _| | \n (_) | \n    \\_\\\n"); // x3
-                        process::exit(1);
-                    },
-                };
-                // tu as besoin d'aide ? - ?
-                match regex.captures(trimmed_line) {
+            for matcher in regexes.iter() {
+                match matcher.1.captures(trimmed_line) {
                     Some(caps) => {
                         trimmed_line = trimmed_line.split_at(caps.get(0).unwrap().as_str().len()).1;
                         match matcher.0 {
                             patpat::Kinds::Space => { /* noop */ },
                             _ => {
                                 let term = Term::from_match(&caps, &matcher.0);
-
                                 println!("{:?}", &term);
-
                                 terms.push(term);
                             }
                         };
@@ -85,6 +88,8 @@ mod patpat {
     }
 
     #[derive(Debug)]
+    #[derive(Copy)]
+    #[derive(Clone)]
     pub enum Kinds { // value-less enum of the different kinds of things that there can be
         Boolean,
         Symbol,

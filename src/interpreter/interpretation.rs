5b9d057bb6794af0fbcabe1396ac293f1fe8600c
Adrien Burgun <adrien.burgun@orange.fr>
2020-06-03 17:47:13 +0200

    :art: src/interpreter/interpretation.rs cleanup

--- src/interpreter/interpretation.rs
+++ src/interpreter/interpretation.rs
@@ -2,16 +2,29 @@ use super::*;
 use std::cell::RefCell;
 use std::collections::HashMap;
 
-pub fn interprete_interpretation<'a>(value: VariableValue<'a>, (into, body): (RStructWeak<'a>, RASTRef<'a>)) -> VariableValue<'a> {
-  let mut init_ctx = Context::from(body.clone());
-  init_ctx.variables.insert(String::from("from"), value);
-  let res = VariableValue::Instance(into.upgrade().unwrap(), Rc::new(RefCell::new(HashMap::new())));
-  init_ctx.variables.insert(String::from("to"), res.clone());
+/** Casts or interpretes `value` into an instance of `into`.
+    @param value - The value to cast
+    @param into - The type to turn `value` into
+    @param body - The (constructed and resolved) interpretation's body
+**/
+pub fn interprete_interpretation<'a>(
+    value: VariableValue<'a>,
+    (into, body): (RStructWeak<'a>, RASTRef<'a>),
+) -> VariableValue<'a> {
+    let mut init_ctx = Context::from(body.clone());
+    init_ctx.variables.insert(String::from("from"), value);
+    let res = VariableValue::Instance(
+        into.upgrade().unwrap(),
+        Rc::new(RefCell::new(HashMap::new())),
+    );
+    init_ctx.variables.insert(String::from("to"), res.clone());
 
-  match body.borrow().instructions.last() {
-    Some((RASTNode::Block(body), _)) => interprete(body.clone(), vec![Rc::new(RefCell::new(init_ctx))]),
-    _ => panic!("Expected interpretation body to end with a block")
-  };
+    match body.borrow().instructions.last() {
+        Some((RASTNode::Block(body), _)) => {
+            interprete(body.clone(), vec![Rc::new(RefCell::new(init_ctx))])
+        }
+        _ => panic!("Expected interpretation body to end with a block"),
+    };
 
-  res
+    res
 }
